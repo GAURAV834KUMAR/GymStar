f7ac008dabf20a600faebcfc34620515c839e9b9
Gaurav Kumar <113380306+GAURAV834KUMAR@users.noreply.github.com>
2024-03-05 23:52:06 +0530

    Update edit_post_widget.dart

--- lib/pages/posts/edit_post/edit_post_widget.dart
+++ lib/pages/posts/edit_post/edit_post_widget.dart
@@ -2,8 +2,12 @@ import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'edit_post_model.dart';
 export 'edit_post_model.dart';
@@ -58,6 +62,15 @@ class _EditPostWidgetState extends State<EditPostWidget> {
 
   @override
   Widget build(BuildContext context) {
+    if (isiOS) {
+      SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(
+          statusBarBrightness: Theme.of(context).brightness,
+          systemStatusBarContrastEnforced: true,
+        ),
+      );
+    }
+
     context.watch<FFAppState>();
 
     return GestureDetector(
@@ -105,7 +118,8 @@ class _EditPostWidgetState extends State<EditPostWidget> {
           ),
           actions: [
             Padding(
-              padding: const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 15.0, 0.0),
+              padding:
+                  const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 15.0, 0.0),
               child: Row(
                 mainAxisSize: MainAxisSize.max,
                 children: [
@@ -160,8 +174,8 @@ class _EditPostWidgetState extends State<EditPostWidget> {
                   ),
                 ),
                 Padding(
-                  padding:
-                      const EdgeInsetsDirectional.fromSTEB(15.0, 12.0, 15.0, 12.0),
+                  padding: const EdgeInsetsDirectional.fromSTEB(
+                      15.0, 12.0, 15.0, 12.0),
                   child: Row(
                     mainAxisSize: MainAxisSize.max,
                     children: [
@@ -190,8 +204,8 @@ class _EditPostWidgetState extends State<EditPostWidget> {
                         ),
                       ),
                       Padding(
-                        padding:
-                            const EdgeInsetsDirectional.fromSTEB(12.0, 0.0, 0.0, 0.0),
+                        padding: const EdgeInsetsDirectional.fromSTEB(
+                            12.0, 0.0, 0.0, 0.0),
                         child: Column(
                           mainAxisSize: MainAxisSize.max,
                           crossAxisAlignment: CrossAxisAlignment.start,
@@ -223,7 +237,8 @@ class _EditPostWidgetState extends State<EditPostWidget> {
                                     context.pushNamed(
                                       'Location',
                                       extra: <String, dynamic>{
-                                        kTransitionInfoKey: const TransitionInfo(
+                                        kTransitionInfoKey:
+                                            const TransitionInfo(
                                           hasTransition: true,
                                           transitionType:
                                               PageTransitionType.bottomToTop,
@@ -320,11 +335,12 @@ class _EditPostWidgetState extends State<EditPostWidget> {
                                 ),
                                 if (widget.post!.taggedUsers.isNotEmpty)
                                   Padding(
-                                    padding: const EdgeInsetsDirectional.fromSTEB(
-                                        6.0, 0.0, 0.0, 0.0),
+                                    padding:
+                                        const EdgeInsetsDirectional.fromSTEB(
+                                            6.0, 0.0, 0.0, 0.0),
                                     child: Text(
                                       () {
-                                        if (widget.post?.taggedUsers.isEmpty) {
+                                        if (widget.post!.taggedUsers.isEmpty) {
                                           return 'Tag users';
                                         } else if (widget
                                                 .post?.taggedUsers.length ==
